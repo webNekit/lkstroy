230480a3cbb1f7c471a2e8c75e4e1c5d2a4366df
webNekit <mochalov-hunter@mail.ru>
2024-07-16 12:15:39 +0300

    create mobile menu

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ const swiper = new Swiper('.swiper', {
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
+
+  // mobile menu
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menuBtn');
+    const closeMenuBtn = document.getElementById('closeMenu');
+    const navbar = document.getElementById('navbar');
+    const body = document.body;
+
+    menuBtn.addEventListener('click', function() {
+        navbar.classList.add('header__navbar--open');
+        body.classList.add('no-scroll');
+    });
+
+    closeMenuBtn.addEventListener('click', function() {
+        navbar.classList.remove('header__navbar--open');
+        body.classList.remove('no-scroll');
+    });
+
+    // Закрытие меню при клике вне его области (дополнительно, если нужно)
+    document.addEventListener('click', function(event) {
+        if (!navbar.contains(event.target) && !menuBtn.contains(event.target)) {
+            navbar.classList.remove('header__navbar--open');
+            body.classList.remove('no-scroll');
+        }
+    });
+});
